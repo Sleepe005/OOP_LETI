4b7ebb41b80231b741151814319cc8412595d693
Sleepe005 <kholopov005@mail.ru>
2025-09-08 15:14:49 +0300

    feat:change sample

--- lab1/array.h
+++ lab1/array.h
@@ -7,10 +7,29 @@ class Array{
         int length = 0;
         T *items;
     public:
-        Array();
-        Array(int len, T *items);
-        void resizeArray(int newLen, T *newItems);
-        void printArray();
-};
+        Array(){
+            length = 0;
+            items = nullptr;
+        }
 
-#include "array.tpp"
+        Array(int len, T *items){
+            this->length=len;
+            this->items = new T[this->length];
+            for(int i = 0; i < length; ++i){
+                this->items[i]=items[i];
+            }
+        }
+        void resizeArray(int newLen, T *newItems){
+            this->length = newLen;
+            delete this->items;
+            this->items = new T[this->length];
+            for(int i = 0; i < length; ++i){
+                this->items[i]=newItems[i];
+            }
+        }
+        void printArray(){
+            for(int i = 0; i < length; ++i){
+                std::cout << items[i] << std::endl;
+            }
+        }
+};
