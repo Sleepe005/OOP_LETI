ee1d0e048345a25180c371302feaa97069f6073e
mat-t0 <mariiamarina70@gmail.com>
2025-09-26 15:10:51 +0300

    Create main.cpp

---
+++ lab2/main.cpp
@@ -0,0 +1,12 @@
+// #include "array.hpp"
+#include "application.hpp"
+#include <iostream>
+
+int main(){
+    Application app;
+
+    // bool work = true;
+    while(app.printMenu());
+    
+    return 0;
+}
